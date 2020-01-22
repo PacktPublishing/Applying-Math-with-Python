b526a78df4ffe511637b37a37a0643ead45829fe
inakleinbottle <sam@inakleinbottle.com>
2020-01-22 16:22:39 +0000

    Added systems of differential equations code

---
+++ ch3/solving-systems-of-differential-equations.py
@@ -0,0 +1,37 @@
+
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def predator_prey_system(t, y):
+    return np.array([5*y[0] - 0.1*y[0]*y[1], 0.1*y[1]*y[0] - 6*y[1]])
+
+
+p = np.linspace(0, 100, 25)
+w = np.linspace(0, 100, 25)
+P, W = np.meshgrid(p, w)
+
+dp, dw = predator_prey_system(0, np.array([P, W]))
+
+fig, ax = plt.subplots()
+ax.quiver(P, W, dp, dw)
+ax.set_title("Population dynamics for two competing species")
+ax.set_xlabel("P")
+ax.set_ylabel("W")
+
+#plt.show()
+
+
+initial_conditions = np.array([85, 40])
+
+from scipy import integrate
+sol = integrate.solve_ivp(predator_prey_system, (0., 5.), initial_conditions, max_step=0.01)
+
+#fig, ax = plt.subplots()
+ax.plot(initial_conditions[0], initial_conditions[1], "ko")
+#ax.set_title("
+ax.plot(sol.y[0, :], sol.y[1, :], "k", linewidth=0.5)
+
+#plt.show()
+fig.savefig("systemodeswithtrajectory.png", dpi=300)
