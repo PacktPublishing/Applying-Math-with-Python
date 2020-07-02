d0908b2c56312aede41852b6d36c257618d50312
Sam Morley <41870650+inakleinbottle@users.noreply.github.com>
2020-07-02 08:53:00 +0100

    Fixed error in ANOVA test and print statement

--- ch6/testing-hypotheses-using-ANOVA.py
+++ ch6/testing-hypotheses-using-ANOVA.py
@@ -10,14 +10,13 @@ process_b = rng.normal(4.5, 2.0, size=64)
 significance = 0.05
 
 
-F_stat, p_value = stats.f_oneway(current, process_b, process_b)
+F_stat, p_value = stats.f_oneway(current, process_a, process_b)
 
 print(f"F stat: {F_stat}, p value: {p_value}")
-# F stat: 0.9454281773142474, p value: 0.3906075104622708
-
+# F stat: 9.949052026027028, p value: 9.732322721019206e-05
 
 if p_value <= significance:
-    print("Accept H0: all means equal")
-else:
     print("Reject H0: there is a difference between means")
+else:
+    print("Accept H0: all means equal")
 # Reject H0: there is a difference between means
