12da534f6af8de8cf914571db768da140ac00f7a
Sam Morley <41870650+inakleinbottle@users.noreply.github.com>
2020-07-14 10:17:41 +0100

    Fixed typo in code

--- ch10/accouting-for-uncertainty-in-calculations.py
+++ ch10/accouting-for-uncertainty-in-calculations.py
@@ -7,7 +7,7 @@ print(seconds)  # 3.0+/-0.4
 depth = 0.5*9.81*seconds*seconds
 print(depth)  # 44+/-12
 
-other_depth = ufloat(45, 12)
+other_depth = ufloat(44, 12)
 time = umath.sqrt(2.0*other_depth/9.81)
 print("Estimated time", time)
 # Estimated time 3.0+/-0.4
